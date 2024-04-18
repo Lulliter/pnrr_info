d6324ce1526dc2ae7ff5ece6ca058a82d85165db
Lulliter <lmm76@georgetown.edu>
2024-04-18 14:24:43 +0200

    site  🤞🏻

---
+++ ___render_deploy.sh
@@ -0,0 +1,74 @@
+#=========================================== (Render site Locally) ================================================#
+
+# ====== Font Awesome Extension for Quarto
+quarto add quarto-ext/fontawesome
+
+# ====== RENDER the entire site
+# quarto preview
+quarto preview
+
+
+# ====== RENDER the entire site
+quarto render
+
+# render a single file only
+project:
+  render:
+    - section1.qmd
+    - section2.qmd
+    - "*.qmd"
+    - "!ignored.qmd"
+    - "!ignored-dir/"
+
+# ====== PUBLISH
+# quarto publish  # quasto stronzo mi crea la git branch "gh-pages" -->  FIX delete branch
+			#git branch -d branch_to_delete # { not allowed IF I am on it}
+			#git branch -D branch_to_delete # { -D if you have changes that are not merged and STILL delete}
+
+#=========================================== (Push to Github repo) ================================================#
+# check status
+git status
+
+# Add changes to git Index.
+git add -A # ALL
+git add -u # tracked
+git add  index.qmd         me.qmd .nojekyll
+git add theme/
+git add images/*
+
+=======
+# Create Std commit "message"....
+msg="rebuilt on `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+# ... Commit Those changes.
+git commit -m "$msg"
+=======
+
+git commit -m "site 3rd try  🤞🏻"
+git commit -m "added _variables.yml & cgd tools/01_git.qmd 🙌"
+		# git commit -m "revision INSTALL + cleanup slides 2"  -m "01_... + 00_carico_tab-contesto.qmd "
+
+# Push local source (master branch) to remote reference (origin)
+#cd .
+git push origin master
+
+
+#=========================================== FIle pubblico  ================================================#
+# https://quarto.org/docs/publishing/quarto-pub.html
+#  from ./
+cd .
+quarto publish quarto-pub 10_Validazione.qmd   # Published at https://lulliter.quarto.pub/validazione-dati-in-regis/
+#-->>>>>>>  (dare ENTER x farlo partire)
+
+# ====== Run Script that copies things
+# PRIMA CHIUDO TUTTO WORD
+Rscript R/salvo_output_li.R
+
+
+#=========================================== (IGNORE a file accidentally committed in the past) ================================================#
+# add .env file to .gitignore
+echo "accident.txt" >> .gitignore
+# tell Git NOT to track this file (it gets removed from the index, but stays local system)
+git rm --cached accident.txt
